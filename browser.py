7a82691577bd9d6e25e094335a4a13d1664a61ad
KeerthiKongari <keerthikongari26@gmail.com>
2020-07-12 22:41:58 +0530

    4/6 stage

--- browser.py
+++ browser.py
@@ -1,74 +1,69 @@
-from collections import deque
-import sys
-import os
- 
-nytimes_com = '''
-This New Liquid Is Magnetic, and Mesmerizing
- 
-Scientists have created “soft” magnets that can flow 
-and change shape, and that could be a boon to medicine 
-and robotics. (Source: New York Times)
- 
- 
-Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
- 
-Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
- 
-'''
- 
-bloomberg_com = '''
-The Space Race: From Apollo 11 to Elon Musk
- 
-It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
- 
- 
-Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
- 
-Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.
-'''
+import sys, os
+import requests
  
 # write your code here
-name_folder = sys.argv[1]
-if not os.path.exists(name_folder):
-    os.mkdir(name_folder)
+def check_url(site):
+    url = site if site.startswith("https://") else "https://" + site
+    return url
  
-history = deque()
-while True:
+def get_data_by_url(url):
+    url = check_url(url)
+    req = requests.get(url)
+    return req.text
+ 
+if len(sys.argv) == 2:
+    directory_name = sys.argv[1]
+ 
+    path = ".\\" + directory_name
+ 
+    if not os.path.isdir(path):
+        os.mkdir(path)
+ 
+# record of browser history
+history = []
+ 
+choice = -1
+while choice != 0:
+ 
     url = input()
+    content = None
  
-    if '.' in url:
-        path = os.path.join(name_folder, '.'.join(url.split('.')[:-1]))
-        if 'bloomberg' in url:
-            history.append('bloomberg')
-            with open(path, 'w', encoding='utf-8') as file:
-                file.write(bloomberg_com)
-                print(bloomberg_com)
-        elif 'nytimes' in url:
-            history.append('nytimes')
-            with open(path, 'w', encoding='utf-8') as file:
-                file.write(nytimes_com)
-                print(nytimes_com)
-        else:
-            print('error')
+    if url == "exit":
+        choice = 0
+ 
+    elif url == "back":
+        history.pop()
+        if history:
+            
+            if history[-1] == "bloomberg":
+                content = bloomberg_com
+ 
+            elif history[-1] == "nytimes":
+                content = nytimes_com
+ 
+            print(content)
+ 
+    elif url.count('.') != 0:
+ 
+        tokens = url.split('.')
+        content = get_data_by_url(url)
+ 
+        print(content)
+ 
+        # add current website into browser history
+        web_name = tokens[0]
+        history.append( web_name )
+ 
+ 
+        with open(path + "/" + web_name, 'w') as f:
+            f.write(content)
+ 
+    elif url in history:
+ 
+        web_name = url
+        with open(path + "/" + web_name, 'r') as f:
+            content = f.read()
+            print(content)
+ 
     else:
-        if url == 'exit':
-            sys.exit()
-        if url == 'back':
-            history.pop()
-            url = history.pop()
-        try:
-            path = os.path.join(name_folder, url)
-            with open(path, 'r', encoding='utf-8') as file:
-                print(file.read())
-        except FileNotFoundError:
-            print('error')
+        print('Error: Incorrect URL')
